d88f32580e4411f3c678222a91d0b8577153655b
Naveen Raman <dsfan414@gmail.com>
2015-07-30 10:17:20 -0400

    Added Home Page for pages, and header+footer

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	url(r'^$',TemplateView.as_view(template_name="core/index.html"))
+	url(r'^page/',include('page.urls')),
+	url(r'^$',TemplateView.as_view(template_name="core/index.html")),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-
-class StaticView(TemplateView):
-	template_name = "index.html"

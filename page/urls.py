1dea468cea9d7f2b300291c3946a6762e3df5f54
Naveen Raman <dsfan414@gmail.com>
2015-07-30 13:24:07 -0400

    Allows for Page Viewing

--- page/urls.py
+++ page/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from django.views.generic.base import TemplateView
 
+from . import views
+
 urlpatterns = [
-	url(r'^$',TemplateView.as_view(template_name="page/page.html")),
+#	url(r'^$',TemplateView.as_view(template_name="page/page.html")),
+	url(r'^(?P<page_id>[0-9]+)/$',views.page,name="page")
 ]
--- page/views.py
+++ page/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Page
+from django.http import HttpResponse
+from django.template import loader,RequestContext
 
 # Create your views here.
+
+def page(request,page_id):
+	p = Page.objects.get(pk=page_id)
+	title = p.title
+	template = loader.get_template("page/page.html")
+	context = RequestContext(request,{'title':title})
+	return HttpResponse(template.render(context))
 
 
